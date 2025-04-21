bd10925fedd3c9912a34d456d451c0dfb2643969
arssimonyan <arsen@windsor.ai>
2025-04-21 17:25:31 +0400

    change port

--- app.js
+++ app.js
@@ -29,5 +29,5 @@ app.use(homeRoutes);
 app.use(userRoutes);
 app.use(taskRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
